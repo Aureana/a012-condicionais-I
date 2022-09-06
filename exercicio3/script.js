6c6ba3ef3a255221db5bd04d389263796ed214ba
Aureana <111321782+Aureana@users.noreply.github.com>
2022-09-06 16:05:27 -0300

    Atividade condicional resolvida.

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,8 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Digite sua nacionalidade, por favor!")//= adicione o prompt aqui
+
+if (nacionalidade === "brasileira" || nacionalidade === "argentina"|| nacionalidade === "uruguaia"|| nacionalidade === "chilena" || nacionalidade === "colombiana"){
+    console.log("Sua nacionaidade é?:",nacionalidade)
+
+}else{
+   console.log("Naturalidade não encontrada")
+}
